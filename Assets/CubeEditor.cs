d5db5b9445c6fd54a18c8e81c21832c23feb2746
imglad91 <glad.poenaru@outlook.com>
2020-08-03 17:14:48 -0600

    Add label & script for grid block location

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+    TextMesh textMesh;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.Round(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.Round(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x + "," + snapPos.z;
+    }
+}
